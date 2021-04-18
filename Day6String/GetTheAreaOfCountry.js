8edbf9bea2d54c10b30b25b46e023f6e37d7c959
chandakumari <chandakumari07071999@gmail.com>
2021-04-18 23:04:57 +0530

    Practice-Js

--- Day6String/GetTheAreaOfCountry.js
+++
@@ -1,7 +0,0 @@
-/*
-Create a function that takes a country's name and its area as arguments and returns the area of the country's proportion of the total world's landmass.
-*/
-function areaOfCountry(name, area) {
-	let ans = (+area*100) / 148_940_000;
-	return `${name} is ${ans.toFixed(2)}% of the total world's landmass`
-}
